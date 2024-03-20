71f5d98b6b0cf10a1cda2d352bf07bf1edeac9b8
Ignacio Mendez Nole <ignaciomendeznole@gmail.com>
2024-03-20 15:55:30 -0300

    Refactor message pattern in PaymentsController

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -8,7 +8,7 @@ import { Request, Response } from 'express';
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
-  @MessagePattern('create_payment_session')
+  @MessagePattern('create.payment.session')
   createPaymentSession(
     @Payload() createPaymentSessionDto: CreatePaymentSessionDto,
   ) {
